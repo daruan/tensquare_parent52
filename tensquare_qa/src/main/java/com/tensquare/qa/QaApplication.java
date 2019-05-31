0c81f2e505d3300e85ac95a77c851593e9af52a4
daruan <1197876010@qq.com>
2019-05-31 09:59:37 +0800

    update
    update

---
+++ tensquare_qa/src/main/java/com/tensquare/qa/QaApplication.java
@@ -0,0 +1,21 @@
+package com.tensquare.qa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+
+/**
+ * Created by daruan on 2019/5/30.
+ */
+@SpringBootApplication
+public class QaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QaApplication.class,args);
+    }
+
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker(1,1);
+    }
+}
